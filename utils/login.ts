68384159e1954782c204402c9977877d4bcd39cf
Raja Sekhar <raja.sekhar@hotmail.co.uk>
2024-07-19 09:03:30 +0100

    change title

--- utils/login.ts
+++ utils/login.ts
@@ -3,7 +3,7 @@ import { expect, type Page  } from '@playwright/test'
 export const login = async (page: Page) => {
     await page.goto('/')
     const pageTitle = 'Swag Labs'
-    await expect(page).toHaveTitle(/Swag Labs/)
+    await expect(page).toHaveTitle(pageTitle)
     await page.fill('#user-name', 'standard_user')
     await page.fill('#password', 'secret_sauce')
     await page.locator('#login-button', { hasText: 'Login' }).click()
